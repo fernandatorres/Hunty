788f4ceae6f369f2e50c7adca6fd215a0f83a1c3
Fernanda <fernandatorres1909@hotmail.com>
2023-06-09 05:04:24 -0500

    Modificacion

--- src/main/java/TestNG/testN.java
+++ src/main/java/TestNG/testN.java
@@ -4,43 +4,43 @@ import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import  org.testng.Asset;
-import org.testng.annotations.*;
+//import  org.testng.Asset;
+//import org.testng.annotations.*;
 
 public class testN {
     private String url = "https://demoqa.com/";
     WebDriver driver;
 
-    @BeforeTest
-    public void setBaseUrl()
-    {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pitufa\\Desktop\\installproyect\\Proyect\\Prueba\\src\\main\\resources\\driver\\chromedriver.exe");
-        driver = new ChromeDriver();
+   // @BeforeTest
+    //public void setBaseUrl()
+    //{
+      //  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pitufa\\Desktop\\installproyect\\Proyect\\Prueba\\src\\main\\resources\\driver\\chromedriver.exe");
+        //driver = new ChromeDriver();
 
-        driver.get(url);
-    }
+        //driver.get(url);
+    //}
 
-    @Test
-    public void titleTest()
-    {
-        String expectedTitle = "ToolsQA";
-        String actualTitle = driver.getTitle();
+    //@Test
+    //public void titleTest()
+    //{
+      //  String expectedTitle = "ToolsQA";
+        //String actualTitle = driver.getTitle();
 
-        Assert.asserEquals(actualTitle,expectedTitle);
-    }
-    @Test
-    public void urlTest()
-    {
-        String actualUrl = driver.getCurrentUrl():
+      //  Assert.asserEquals(actualTitle,expectedTitle);
+    //}
+    //@Test
+    //public void urlTest()
+    //{
+      //  String actualUrl = driver.getCurrentUrl():
 
-        Assert.asserEquals(actualUrl, url);
+        //Assert.asserEquals(actualUrl, url);
 
-    }
-    @AfterTest
-    public void closeSessioin ()
-    {
-        driver.quit();
-    }
+    //}
+    //@AfterTest
+    //public void closeSessioin ()
+    //{
+      //  driver.quit();
+    //}
 
 
 
